3acd485c90a1686885e6f8ca73f9ce94ba18aa91
장연식 <sseregit@knowwheresoft.com>
2025-04-21 17:18:47 +0900

    Kafka Producer 구현하기

--- go-gateway/go.mod
+++ go-gateway/go.mod
@@ -1,3 +1,5 @@
 module go-gateway
 
 go 1.24
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
--- go-gateway/kafka/producer.go
+++ go-gateway/kafka/producer.go
@@ -1,1 +1,60 @@
 package kafka
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"go-gateway/config"
+	"log"
+)
+
+const (
+	_allAcks = "all"
+)
+
+type Producer struct {
+	cfg      config.Producer
+	producer *kafka.Producer
+}
+
+func NewProducer(cfg config.Producer) Producer {
+	url := cfg.URL
+	id := cfg.ClientId
+	acks := cfg.Acks
+
+	if acks == "" {
+		acks = _allAcks
+	}
+
+	conf := &kafka.ConfigMap{
+		"bootstrap.servers": url,
+		"client.id":         id,
+		"acks":              acks,
+	}
+
+	if producer, err := kafka.NewProducer(conf); err != nil {
+		panic(err.Error())
+	} else {
+		return Producer{
+			cfg:      cfg,
+			producer: producer,
+		}
+	}
+}
+
+func (p Producer) SendEvent(v []byte) {
+	topic := p.cfg.Topic
+
+	err := p.producer.Produce(
+		&kafka.Message{
+			TopicPartition: kafka.TopicPartition{
+				Topic:     &topic,
+				Partition: kafka.PartitionAny,
+			},
+			Value: v,
+		}, nil)
+
+	if err != nil {
+		log.Println("Failed to send topic", string(v))
+	} else {
+		log.Println("Success to send message", string(v))
+	}
+}
