b2852067d3a3926bec48a6fc79bc964c79058a44
장연식 <sseregit@knowwheresoft.com>
2025-04-21 16:40:16 +0900

    가장 기본적인 환경 변수를 위한 코드 작업

---
+++ go-gateway/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+type Config struct {
+	App []App `yaml:"app"`
+}
+
+type App struct {
+	App struct {
+		Port    string `yaml:"port"`
+		Version string `yaml:"version"`
+	} `yaml:"app"`
+}
---
+++ go-gateway/go.mod
@@ -0,0 +1,3 @@
+module go-gateway
+
+go 1.24
